7507436aec09681f93c1890a184cbf7d904f790b
Saksham Shrivastava <sakshamshrivastav58@gmail.com>
2025-01-31 13:35:26 +0530

    Update index.js

--- ecommerce-backend/index.js
+++ ecommerce-backend/index.js
@@ -27,7 +27,7 @@ app.use(cors({
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://3legant-frontend.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });

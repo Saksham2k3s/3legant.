b2deab64a0e691e02c0e651a7cd517effa64bdff
Saksham2k3s <sakshamshrivastav58@gmail.com>
2025-03-07 01:19:30 +0530

    Added price filter

--- ecommerce-frontend/src/helpers/product.js
+++ ecommerce-frontend/src/helpers/product.js
@@ -1,3 +1,20 @@
 // getCacheKey to construct a cache key
-export const getCacheKey = (page, query, category) =>
-  `${page}-${query}-${category}`;
+export const getCacheKey = (page, query, category, minPrice, maxPrice) =>
+  `${page}-${query}-${category}-${minPrice}-${maxPrice}`;
+
+
+// Helper function to construct query parameters string
+export const buildQueryParams = (params) => {
+  const queryParams = new URLSearchParams();
+
+  // Only append parameters if they have values
+  if (params.page) queryParams.append('page', params.page);
+  if (params.query) queryParams.append('keyword', params.query);
+  if (params.category) queryParams.append('category', params.category);
+
+    // Append price filter if provided
+    if (params.minPrice) queryParams.append("minPrice", params.minPrice);
+    if (params.maxPrice) queryParams.append("maxPrice", params.maxPrice);
+
+  return queryParams.toString();
+};

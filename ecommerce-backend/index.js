d87a46d064e52eb8ff39f93363b2b4309ed1df2c
Saksham2k3s <sakshamshrivastav58@gmail.com>
2025-11-16 13:23:05 +0530

    added changes for cores error in backend

--- ecommerce-backend/index.js
+++ ecommerce-backend/index.js
@@ -1,36 +1,32 @@
-const bodyParser = require('body-parser');
-const cartRoute = require('./routes/cartRoute');
-const cloudinary = require('cloudinary');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const databaseConnection = require('./config/dbConnet');
-const dotenv = require('dotenv');
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const orderRoute = require('./routes/orderRoute');
-const productRoute = require('./routes/productRoute');
-const userRoute = require('./routes/userRoute');
-const wishlistRoute = require('./routes/wishlistRoute');
+const bodyParser = require("body-parser");
+const cartRoute = require("./routes/cartRoute");
+const cloudinary = require("cloudinary");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const databaseConnection = require("./config/dbConnet");
+const dotenv = require("dotenv");
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const orderRoute = require("./routes/orderRoute");
+const productRoute = require("./routes/productRoute");
+const userRoute = require("./routes/userRoute");
+const wishlistRoute = require("./routes/wishlistRoute");
 
-// Initailize app 
+// Initailize app
 const app = express();
 // Config
-dotenv.config({ path: './.env' });
+dotenv.config({ path: "./.env" });
 
 // Configure CORS
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CLIENT_URL,
-    methods: 'GET,POST,PUT,DELETE,PATCH', 
-    credentials: true, 
-  }));
+    methods: "GET,POST,PUT,DELETE,PATCH",
+    credentials: true,
+  })
+);
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
+app.options("*", cors());
 
 // Connect to Database
 databaseConnection();
@@ -39,24 +35,24 @@ databaseConnection();
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET_KEY
+  api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
 });
 
 // Middleware
-app.use(express.json()); 
+app.use(express.json());
 app.use(cookieParser());
 app.use(fileUpload());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Router import
-app.use('/api/v1/user', userRoute); 
-app.use('/api/v1/product', productRoute);
-app.use('/api/v1/order', orderRoute);
-app.use('/api/v1/cart', cartRoute);
-app.use('/api/v1/wishlist', wishlistRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/product", productRoute);
+app.use("/api/v1/order", orderRoute);
+app.use("/api/v1/cart", cartRoute);
+app.use("/api/v1/wishlist", wishlistRoute);
 
 // Start the server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-    console.log(`Backend is running on port ${PORT}`);
+  console.log(`Backend is running on port ${PORT}`);
 });
